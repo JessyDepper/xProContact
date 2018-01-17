f9beced58f98459fd8d7928b7439b55c4383c27e
JessyDepper <minhnk.1607@gmail.com>
2018-01-17 09:28:27 +0700

    first commit

---
+++ iOS/xProContact/xProContact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xProContact
+//
+//  Created by Jessy on 1/13/18.
+//  Copyright © 2018 JD Join-stock Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/xProContact/xProContact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xProContact
+//
+//  Created by Jessy on 1/13/18.
+//  Copyright © 2018 JD Join-stock Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
